e20e6a4c9d1d2882c73de5b1bbad6947a50f8d6d
felipematheus1337 <lipehbr@gmail.com>
2025-12-06 10:34:10 -0300

    Adicionado configuração do postgresql

--- config/postgres.go
+++ config/postgres.go
@@ -5,43 +5,42 @@ import (
 	"os"
 
 	"github.com/felipematheus1337/GoPHER_Blog/schemas"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitializePostgres() (*gorm.DB, error) {
 
-	dbPath := "./db/main.db"
-
-	_, err := os.Stat(dbPath)
-
-	if os.IsNotExist(err) {
-		err = os.MkdirAll("./db", os.ModePerm)
-
-		if err != nil {
-			return nil, err
-		}
-
-		file, err := os.Create(dbPath)
-
-		if err != nil {
-			return nil, err
-		}
-
-		file.Close()
-	}
-
-	db, err := gorm.Open(postgres.Open(dbPath), &gorm.Config{})
+	err := godotenv.Load()
 
 	if err != nil {
-		return nil, fmt.Errorf("error initializing database: %v", err)
+		fmt.Println("Error loading .env file")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, fmt.Errorf("Error connecting to database: %v", err)
 	}
 
 	err = db.AutoMigrate(&schemas.Post{})
 
 	if err != nil {
-		return nil, fmt.Errorf("error creating the schema of the  database: %v", err)
+		return nil, fmt.Errorf("Error auto-migrating database: %v", err)
 	}
 
 	return db, nil
+
 }
