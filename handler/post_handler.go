e07c6e9ae4ce3ea6bb5d6d78776070f8fbf84c2a
felipematheus1337 <lipehbr@gmail.com>
2025-12-06 11:25:38 -0300

    Iniciado camada de service.

--- handler/post_handler.go
+++ handler/post_handler.go
@@ -1,12 +1,16 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/felipematheus1337/GoPHER_Blog/service"
+	"github.com/gin-gonic/gin"
+)
 
 type PostHandler struct {
+	service *service.PostService
 }
 
-func NewPostHandler() *PostHandler {
-	return &PostHandler{}
+func NewPostHandler(service *service.PostService) *PostHandler {
+	return &PostHandler{service}
 }
 
 func (p *PostHandler) CreatePost(c *gin.Context) {
