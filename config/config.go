e183d3369259a455d34d7dc686709fda2fb124e4
felipematheus1337 <lipehbr@gmail.com>
2025-12-06 10:04:09 -0300

    Criado classe de configuração, chamado inicialização do banco de dados.

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Init() error {
+	var err error
+
+	db, err = InitializePostgres()
+
+	if err != nil {
+		return fmt.Errorf("error initializing database: %v", err)
+	}
+
+	return nil
+}
